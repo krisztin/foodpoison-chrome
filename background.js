98b36b896dd54a81dfd5109f11ebd6e5868b1e01
krisztin <me@kriszt.in>
2019-09-23 16:30:04 +0200

    background.js clean up and constricting to /menu

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
+const rules = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: 'deliveroo.co.uk/menu'},
+    })],
+  actions: [new chrome.declarativeContent.ShowPageAction()]
+}
+
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("Hello, fellow dev!");
-  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'deliveroo.co.uk'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+    chrome.declarativeContent.onPageChanged.addRules([rules]);
   });
 });
